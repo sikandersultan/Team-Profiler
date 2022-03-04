e331725c2872ce88f5b9f77f392331577df2f040
sikandersultan <sikandersultanmirza@gmail.com>
2022-03-04 10:57:48 +0000

    almost

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const Intern = require("./lib/Intern.js")
 const inquirer = require("inquirer")
 const fs = require("fs")
 const path = require("path")
+const OUTPUT_DIR = path.resolve(__dirname, "output")
+const outputPath = path.join(OUTPUT_DIR, "index.html")
 const newHtml = require("./src/template.js")
 const { choices } = require("yargs")
 
@@ -95,6 +97,36 @@ function theBigFunction () {
         .then (answers => {
             const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGH)
             bigArray.push(engineer)
+            chooseTeam()
+        })
+    }
+
+    function newIntern () {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "internName",
+                message: "Please enter the intern's name"
+            },
+            {
+                type: "input",
+                name: "internID",
+                message: "Please enter the intern's ID number"
+            },
+            {
+                type: "input",
+                name: "internEmail",
+                message: "Please enter the intern's email address"
+            },
+            {
+                type: "input",
+                name: "internSchool",
+                message: "Please enter the intern's school name"
+            }
+        ])
+        .then (answers => {
+            const intern = new Intern(answers.internName, answers.internID, answers.internEmail, answers.internSchool)
+            bigArray.push(intern)
             chooseTeam()
         })
     }
