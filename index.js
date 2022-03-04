95acb5f2fb2bff4293671962ca4bba9e223d1222
sikandersultan <sikandersultanmirza@gmail.com>
2022-03-04 08:16:32 +0000

    adding java

---
+++ index.js
@@ -0,0 +1,6 @@
+const Manager = require("./lib/Manager.js")
+const Engineer = require("./lib/Engineer.js")
+const Intern = require("./lib/Intern.js")
+const inquirer = require("inquirer")
+const fs = require("fs")
+const newHtml = require("./src/template.js")
